1359fd0bdfeb2ae34b6d83e28f05d934dfb8268b
iccsantos00 <80125834+iccsantos00@users.noreply.github.com>
2024-06-18 16:36:41 -0300

    Projeto Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //FormsModule,
+    //HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+  @Component({
+    selector: 'meu-primeiro-component',
+    template: `
+      <p>O IsaMeu primeiro component no Angular 2!</p>
+    ` 
+  })
+  export class MeuPrimeiroComponent { }
